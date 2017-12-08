39abd909778f9ddaee59efe20a9ba998caff336f
gnmarvin <nathaneilmarvin96@gmail.com>
2017-12-08 18:58:22 +0900

    T

--- application/views/pages/register_user.php
+++ application/views/pages/register_user.php
@@ -10,23 +10,25 @@
 <body>
 	<?php echo $header;?>
 	 <?php echo form_open('home/register'); ?>
+        <div class="col-sm-6">
           Email : <input type="text" name="email" value="" size="50" class="form-control"/> <br />
           <?php echo form_error('email', '<p class="text-danger">', '</p>'); ?>
-          Username : <input type="text" name="username" value="" size="50" class="form-control"/> <br />
+          Username : Must be at least 4 characters <input type="text" name="username" value="" size="50" class="form-control"/> <br />
           <?php echo form_error('username', '<p class="bg-danger">', '</p>'); ?>
-          First Name : <input type="text" name="first_name" value="" size="50" class="form-control"/> <br />
+          First Name : Must be at least 2 characters <input type="text" name="first_name" value="" size="50" class="form-control"/> <br />
           <?php echo form_error('first_name', '<p class="bg-danger">', '</p>'); ?>
-          Last Name : <input type="text" name="last_name" value="" size="50" class="form-control"/> <br />
+          Last Name : Must be at least 2 characters<input type="text" name="last_name" value="" size="50" class="form-control"/> <br />
           <?php echo form_error('last_name', '<p class="bg-danger">', '</p>'); ?>
-          Password : <input type="text" name="password" value="" size="50" class="form-control"/> <br />
+          Password : Must be at least 6 characters<input type="text" name="password" value="" size="50" class="form-control"/> <br />
           <?php echo form_error('password', '<p class="bg-danger">', '</p>'); ?>
-          Salt : <input type="text" name="salt" value="" size="50" class="form-control"/> <br />
+          Salt : Must be at least 3 characters<input type="text" name="salt" value="" size="50" class="form-control"/> <br />
           <?php echo form_error('salt', '<p class="bg-danger">', '</p>'); ?>
-          Address : <input type="text" name="address" value="" size="50" class="form-control"/> <br />
+          Address : Must be at least 4 characters<input type="text" name="address" value="" size="50" class="form-control"/> <br />
           <?php echo form_error('address', '<p class="bg-danger">', '</p>'); ?>
-          Phone : <input type="text" name="phone" value="" size="50" class="form-control"/> <br />
+          Phone : Must be at least 10 characters<input type="text" name="phone" value="" size="50" class="form-control"/> <br />
           <?php echo form_error('phone', '<p class="bg-danger">', '</p>'); ?>
-     <?php echo form_submit("register","register"); ?>
+     <?php echo form_submit("register","register", 'class="btn btn-primary"'); ?>
+        </div>
      <?php echo form_close(); ?>
 
 	<?php echo $footer;?>
